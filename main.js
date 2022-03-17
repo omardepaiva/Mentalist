6d8d46b12f1bda1c0234cbcb92d2981dcb385f90
Omar de Paiva <omar.paiva@gmail.com>
2022-03-17 13:06:25 -0300

    Mentalist

---
+++ main.js
@@ -0,0 +1,18 @@
+const numeroSecreto = parseInt(Math.random() * 11);
+
+function Chutar() {
+  const elementoResultado = document.getElementById("resultado");
+  const chute = parseInt(document.getElementById("valor").value);
+  console.log(chute);
+  if (chute == numeroSecreto) {
+    elementoResultado.innerHTML = "Você acertou!!!";
+  } else if (chute > 10 || chute < 0) {
+    elementoResultado.innerHTML = "Você deve digitar um número de 0 a 10";
+  } else {
+    elementoResultado.innerHTML = "Errou";
+  }
+}
+
+
+
+
